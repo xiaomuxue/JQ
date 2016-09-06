6ae86157bf3630b7f4b169fa2520969312620de2
xiaomuxue <1017579156@qq.com>
2016-09-06 17:21:42 +0800

    Upper and lower frame alternate

---
+++ JQuery选择器/案例/1_上下架/js/shopping.js
@@ -0,0 +1,41 @@
+//$(function(){
+	
+	/* var Items=$("table tr td.shopping_product_list_6").children().click(function(){
+	      if( this.innerHTML=="[上架]" ){
+		    this.innerHTML="[下架]";
+			$("table tr td.shopping_product_list_6").children().parent().prev().replaceWith("<td class='shopping_product_list_5'  style='color:green'>已上架</td>");
+		  }else{
+		    this.innerHTML="[上架]";
+			$("table tr td.shopping_product_list_6").children().parent().prev().replaceWith("<td class='shopping_product_list_5'  style='color:red'>已下架</td>");
+		  }  
+	 });*/
+	 
+	 //方法一：
+	 /*$('.shopping_product_list_6 a').click(function(){
+		if( $(this).parent().prev().text()=='已上架' ){
+			$(this).parent().prev().text("已下架").css("color","red");
+			$(this).text('[上架]');
+		}else{
+			$(this).parent().prev().text("已上架").css("color","green");
+			$(this).text('[下架]');
+		}
+	})*/
+   
+//});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
